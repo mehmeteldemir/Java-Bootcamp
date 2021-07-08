69c6c614114703ce05c824eb62121f3eef8303d9
bedirhannbayrak <bedirhan.bayrak41@gmail.com>
2021-07-09 02:18:55 +0300

    day 7

--- day6/northwind/src/main/java/kodlamaio/northwind/api/controller/ProductsController.java
+++ day6/northwind/src/main/java/kodlamaio/northwind/api/controller/ProductsController.java
@@ -1,11 +1,13 @@
 package kodlamaio.northwind.api.controller;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.core.utilities.results.SuccessDataResult;
+import kodlamaio.northwind.core.utilities.results.SuccessResult;
 import kodlamaio.northwind.entity.concrete.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,7 +23,13 @@ public class ProductsController {
     }
 
     @GetMapping()
-    public List<Product> getAll() {
+    public DataResult<List<Product>> getAll() {
         return productService.getAll();
     }
+
+    @PostMapping("/")
+    public Result add(@RequestBody Product product){
+        return productService.save(product);
+    }
+
 }
--- day6/northwind/src/main/java/kodlamaio/northwind/business/concrete/ProductManager.java
+++ day6/northwind/src/main/java/kodlamaio/northwind/business/concrete/ProductManager.java
@@ -1,14 +1,20 @@
 package kodlamaio.northwind.business.concrete;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.core.utilities.results.SuccessDataResult;
+import kodlamaio.northwind.core.utilities.results.SuccessResult;
 import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
 import kodlamaio.northwind.entity.concrete.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Component
+
+@Service
 public class ProductManager implements ProductService {
 
     private ProductDao productDao;
@@ -19,7 +25,13 @@ public class ProductManager implements ProductService {
     }
 
     @Override
-    public List<Product> getAll() {
-        return productDao.findAll() ;
+    public DataResult<List<Product>>  getAll() {
+        return new SuccessDataResult<List<Product>>(productDao.findAll(), "success");
+    }
+
+    @Override
+    public Result save(Product product) {
+        productDao.save(product);
+        return new SuccessResult(product.getProductName() + " saved successfully");
     }
 }
